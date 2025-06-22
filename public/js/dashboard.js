dcdff8bce8fc357b7691f6b2ea225aa9288b6687
Techsmithjenkins <juristjenkins@gmail.com>
2025-06-22 21:04:21 +0000

    Update dashboard.js
    Reverted all previous attempts to force scrolling in dashboard.html

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,7 +2,6 @@
 import { supabase } from './supabaseClient.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  document.body.style.height = '200vh';
   // 1) Redirect if not logged in
   const { data: { session } } = await supabase.auth.getSession();
   if (!session) return window.location.href = 'login.html';
